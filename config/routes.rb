f8d65ee83cbec0f2fc0d17d72269134dbcf0a02f
Shana L Moore <shana.lavina.moore@gmail.com>
2016-03-25 13:53:11 -0700

    add category route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'pins#index'
 
   resources :pins
+  resources :categories, only: [:index]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
